b1fa3f25069c3af9a2ae14d8d801b2819f815db1
A-rya-n <aryan31.purohit@gmail.com>
2023-06-05 17:36:03 +0530

    adding dashboard

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,9 +2,13 @@ import LineMetrics from "./modules/charts/Line";
 
 function App() {
   return (
-    <div className="bg-black h-screen flex justify-center items-center">
-      <div className="bg-slate-700 w-4/5 h-4/5 rounded-lg shadow-xl flex justify-center items-center">
-        <LineMetrics />
+    <div className="bg-slate-700 h-screen">
+      <div className="bg-black w-full h-20 shadow-xl mb-5"></div>
+      <div className="flex gap-6">
+        <div className="bg-slate-400 w-1/4 h-96 rounded-lg shadow-xl ml-3"></div>
+        <div className="bg-slate-400 w-3/4 h-full rounded-lg shadow-xl mr-3 p-5">
+          <LineMetrics />
+        </div>
       </div>
     </div>
   );
